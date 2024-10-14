1cec8d0c7a507c5cc8a4c5cdf9afdef766278ddd
Andrii Babak <andreybabak1@icloud.com>
2024-10-14 14:39:59 +0100

    Initial Commit

---
+++ TimeConverter/TimeConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeConverterApp.swift
+//  TimeConverter
+//
+//  Created by Andrey Babak on 14/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct TimeConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
